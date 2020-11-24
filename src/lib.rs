04683089d0c3cefdf82fe71cb3b26d8550766919
kento tajiri <k.denjiry@gmail.com>
2020-11-24 12:56:36 +0900

    capture stderr

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,14 @@ pub fn lightblue(input: &str) {
     let command = format!("echo {} | lightblue parse -s xml", input);
     let captured = Exec::shell(command)
         .stdout(Redirection::Pipe)
+        .stderr(Redirection::Pipe)
         .capture()
-        .expect("lightblue maybe exists");
+        .expect("lightblue should exists");
+    let err = &captured.stderr_str();
     let xml_str = captured.stdout_str();
+    dbg!(&err.is_empty());
+    dbg!(err);
+    dbg!(&xml_str);
     let xml_struct = xml_parse::parse(&xml_str);
     dbg!(xml_struct);
 }
