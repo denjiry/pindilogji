035d102b838d9ce236ab09d11e6a7635e6eb2095
kento tajiri <k.denjiry@gmail.com>
2020-11-28 15:40:29 +0900

    fix error & panic handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod xml_parse;
-use std::io::{Error, ErrorKind};
+use std::io;
 use subprocess::{Exec, Redirection};
 
-pub fn lightblue(input: &str) -> Result<(), Error> {
+pub fn lightblue(input: &str) -> Result<(), io::Error> {
     let command = format!("echo {} | lightblue parse -s xml", input);
     let captured = Exec::shell(command)
         .stdout(Redirection::Pipe)
@@ -12,12 +12,14 @@ pub fn lightblue(input: &str) -> Result<(), Error> {
     let err = &captured.stderr_str();
     let xml_str = captured.stdout_str();
     if !err.is_empty() {
-        return Err(Error::new(
-            ErrorKind::Other,
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
             format!("lightblue error: {}", err),
         ));
     }
-    let xml_struct = xml_parse::parse(&xml_str);
+    let xml_struct = xml_parse::parse(&xml_str)
+        .expect("xml parsing should success (assuming correct xml_structure)");
+
     dbg!(xml_struct);
     Ok(())
 }
