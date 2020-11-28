a8b34adadd115e69116db6266dcd9fc09e7ac53e
kento tajiri <k.denjiry@gmail.com>
2020-11-28 13:43:29 +0900

    add Result

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 mod xml_parse;
+use std::io::{Error, ErrorKind};
 use subprocess::{Exec, Redirection};
 
-pub fn lightblue(input: &str) {
+pub fn lightblue(input: &str) -> Result<(), Error> {
     let command = format!("echo {} | lightblue parse -s xml", input);
     let captured = Exec::shell(command)
         .stdout(Redirection::Pipe)
@@ -11,8 +12,12 @@ pub fn lightblue(input: &str) {
     let err = &captured.stderr_str();
     let xml_str = captured.stdout_str();
     dbg!(&err.is_empty());
+    if err.is_empty() {
+        return Err(Error::new(ErrorKind::Other, "lightblue error"));
+    }
     dbg!(err);
     dbg!(&xml_str);
     let xml_struct = xml_parse::parse(&xml_str);
     dbg!(xml_struct);
+    Ok(())
 }
