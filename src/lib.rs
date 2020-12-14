d2275d539517c8188f9b61c62a184560e2f02ea2
kento tajiri <k.denjiry@gmail.com>
2020-12-14 17:19:21 +0900

    change lightblue() to output semantic representation

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,15 @@ use subprocess::{Exec, Redirection};
 
 pub fn lightblue(
     input: &str,
-) -> Result<xml_parse::Sentence, Box<dyn std::error::Error + Send + Sync + 'static>> {
-    let command = format!("echo {} | lightblue parse -s xml", input);
+) -> Result<String, Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let command = format!("echo {} | lightblue parse -s text", input);
     let captured = Exec::shell(command)
         .stdout(Redirection::Pipe)
         .stderr(Redirection::Pipe)
         .capture()
         .expect("lightblue should exists");
     let command_err = &captured.stderr_str();
-    let xml_str = captured.stdout_str();
+    let output = captured.stdout_str();
     if !command_err.is_empty() {
         return Err(io::Error::new(
             io::ErrorKind::Other,
@@ -19,5 +19,5 @@ pub fn lightblue(
         )
         .into());
     }
-    Ok("a")
+    Ok(output)
 }
