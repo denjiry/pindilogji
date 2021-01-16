97f858c44fa664fcf78decaf2cfd5088ec882740
kento tajiri <k.denjiry@gmail.com>
2021-01-16 19:01:00 +0900

    publish pindilogji-ui from pindilogji-server

--- pindilogji-server/src/main.rs
+++ pindilogji-server/src/main.rs
@@ -1,9 +1,25 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_files as acfs;
+use actix_web::http::StatusCode;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
 use pindilogji::{format_sr, lightblue};
 
 #[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index() -> Result<HttpResponse> {
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../../pindilogji-ui/index.html")))
+}
+
+#[get("/pkg/package.js")]
+async fn app() -> Result<acfs::NamedFile> {
+    Ok(acfs::NamedFile::open("../pindilogji-ui/pkg/package.js")?)
+}
+
+#[get("/pkg/package_bg.wasm")]
+async fn app2() -> Result<acfs::NamedFile> {
+    Ok(acfs::NamedFile::open(
+        "../pindilogji-ui/pkg/package_bg.wasm",
+    )?)
 }
 
 #[actix_web::main]
@@ -11,6 +27,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
+            .service(app)
+            .service(app2)
     })
     .bind("127.0.0.1:8080")?
     .run()
